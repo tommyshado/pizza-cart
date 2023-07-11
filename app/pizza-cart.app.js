63f56c1562764e7a3294f9cc4cebf8f35ff59c6a
Mthunzi Tom <mthunzitom.10@gmail.com>
2023-07-11 12:19:13 +0200

    added pseudo code for the logic and test cases, added images for the app

---
+++ app/pizza-cart.app.js
@@ -0,0 +1,24 @@
+
+function pizzaCart() {
+    // Return statement
+    return {
+        // Create a property to keep track of the total pizzas added to the pizza cart
+
+        // Create a property and map the property with the object that contains the price of the different pizzas and
+        // the number of pizzas available then...
+
+        // Create a function that adds a small pizza to the pizza cart, the function should add the price of the pizza
+        // and decrement the number of pizzas in stock, the number of pizzas in stock will decrement only when the pizza is available then...
+
+        // Create a function that adds a medium pizza to the pizza cart, the function should add the price of the pizza
+        // and decrement the number of pizzas in stock, the number of pizzas in stock will decrement only when the pizza is available then...
+
+        // Create a function that adds a large pizza to the pizza cart, the function should add the price of the pizza
+        // and decrement the number of pizzas in stock, the number of pizzas in stock will decrement only when the pizza is available then...
+
+        // Create a function that removes the price of pizzas added to pizzaAddedToCart property then increment the number of pizza in stock accordingly then...
+
+        // Create a function that allows a user to make a payment. Payment can only be made if the amount that the user pay with is enough and...
+        // Decrement the in stock value for a small, medium and large pizza permanently.
+    };
+};
---
+++ unit-testing/pizza-cart.test.js
@@ -0,0 +1,31 @@
+// Write test cases for:
+
+    // ADDING PIZZAS IN THE PIZZA CART
+
+        // when the user add a small pizza to the pizza cart, the total property value should update with price of the small pizza then...
+        // decrement the in stock value for the small pizza
+
+        // when the user add a medium pizza to the pizza cart, the total property value should update with price of the medium pizza then...
+        // decrement the in stock value for the medium pizza
+        
+        // when the user add a large pizza to the pizza cart, the total property should update with price of the large pizza
+        // decrement the in stock value for the large pizza
+
+    // REMOVING PIZZAS FROM THE PIZZA CART
+
+        // when the user removes a small pizza from the pizza cart, the total property value should decrement by the small pizza price then...
+        // the in stock value should increment by 1
+
+        // when the user removes a medium pizza from the pizza cart, the total property value should decrement by the medium pizza price then...
+        // the in stock value should increment by 1
+
+        // when the user removes a large pizza from the pizza cart, the total property value should decrement by the large pizza price then...
+        // the in stock value should increment by 1
+
+    // PIZZA PAYMENT
+
+        // when the user decides to buy a pizza, should be able to allow them to make a purchase when the amount of pizza is enough then...
+        // the in stock value for small, medium and large pizza should be be one less
+
+        // should be able to display a pop message "Enjoy your pizza." when a purchase is successfull otherwise...
+        // should be able to display "Sorry - that is not enough money!".
